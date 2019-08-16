367ad4c5837d7331e100d25d29f6d7fafd66ea7b
genku-m <mi32n1@hotmail.co.jp>
2019-08-16 15:30:55 +0900

    first commit

---
+++ lexer/lexer_test.go
@@ -0,0 +1,37 @@
+package lexer
+
+import (
+	"github/genku-m/monkey/token"
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+
+	for i, tt := range tests  {
+		tok := l.NextToken()
+
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("test[%d] - tokentype wrong.expected=%q,got=%q",i tt.expectedType,tok.Type)
+		}
+
+		if tok.Literal != expectedLiteral {
+			t.Fatalf("test[%d] - literal wrong.expected=%q,got=%q",i tt.expectedLiteral,tok.Literal)
+		}
+	}
+}
